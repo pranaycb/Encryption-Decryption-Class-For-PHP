b8660f9b2f7e19743b86ae559f0922d7bc9c75a2
Pranay Chaktaborty <65500444+pranaycb@users.noreply.github.com>
2023-07-30 19:53:19 +0600

    Initial commit

---
+++ EncryptDecrypt.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Standard encryption & decryption class for php application
+ * @author : Pranay Chakraborty
+ * @link : https://github.com/pranaycb
+*/
+
+define("encryption_method", "AES-128-CBC");
+define("key", "coding_is_fun"); //you can define according to your choice
+
+class EncryptDecrypt 
+{
+    function encrypt($data)
+    {
+        $key = key;
+        $plaintext = $data;
+        $ivlen = openssl_cipher_iv_length($cipher = encryption_method);
+        $iv = openssl_random_pseudo_bytes($ivlen);
+        $ciphertext_raw = openssl_encrypt($plaintext, $cipher, $key, $options = OPENSSL_RAW_DATA, $iv);
+        $hmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary = true);
+        $ciphertext = base64_encode($iv . $hmac . $ciphertext_raw);
+        return $ciphertext;
+    }
+
+    function decrypt($data)
+    {
+        $key = key;
+        $c = base64_decode($data);
+        $ivlen = openssl_cipher_iv_length($cipher = encryption_method);
+        $iv = substr($c, 0, $ivlen);
+        $hmac = substr($c, $ivlen, $sha2len = 32);
+        $ciphertext_raw = substr($c, $ivlen + $sha2len);
+        $original_plaintext = openssl_decrypt($ciphertext_raw, $cipher, $key, $options = OPENSSL_RAW_DATA, $iv);
+        $calcmac = hash_hmac('sha256', $ciphertext_raw, $key, $as_binary = true);
+        if (hash_equals($hmac, $calcmac))
+        {
+            return $original_plaintext;
+        }
+    }
+}
